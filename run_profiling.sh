32a63067bac00394b59016193653c846532d42ea
Kiriaevi <fra977502@gmail.com>
2025-11-16 15:14:47 +0100

    aggiunto script bash di base per effettuare il profiling dell'app con lo strumento perf

---
+++ run_profiling.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+EXECUTABLE="./main"
+
+if [ -f "$EXECUTABLE" ]; then
+    perf record "$EXECUTABLE"
+    perf report
+else
+    echo "Errore: Eseguibile $EXECUTABLE non trovato." >&2
+    exit 1
+fi
