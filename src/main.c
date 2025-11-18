d780e48a1bad5d4864adc5c419cd6053cded21b2
Kiriaevi <fra977502@gmail.com>
2025-11-18 16:09:42 +0100

    + aggiunto scheletro delle funzioni discusse

--- src/main.c
+++ src/main.c
@@ -1,6 +1,63 @@
 #include <stdio.h>
+#include <string.h>
 
+/**
+ * Legge da un file e restituisce un array di float 
+ * INPUT: string s, contenente il path da leggere 
+ * OUTPUT: puntatore ad un array di float di dimensione D 
+ */
+float* readFile(char *f) {
+  return NULL;
+}
+/** 
+ * INPUT: array v di dimensione D, parametro x che indica quanti massimi in v.assoluto cercare
+ * OUTPUT: restituisce due array di interi v+ e v-, inizializzata con 0 e 1 nella posizione dei massimi trovati
+ */
+void quantizing() {
 
+}
+/**
+ * ordina un array in valore assoluto
+ * INPUT: arr, array di float da ordinare
+ * OUTPUT: outArr, array ordinato
+ */
+void ordinamento() {
+}
+
+/**
+ * Dati due vettori v e w ne effettua il prodotto scalare
+ * INPUT: array di float v, array di float w 
+ * OUTPUT: un float che restituisce la distanza
+ */
+float prodScalare(float *v, float *w) {
+  return 0.0f;
+}
+
+/**
+ * Calcola un vettore di pivot
+ * INPUT: array di float v, intero h 
+ * OUTPUT: array di float p di pivot
+ */
+float* calcoloPivot(float *v, int h) {
+  return NULL;
+}
+
+/**
+ * Esegue una query sull'algoritmo 
+ * INPUT: float q[i], singola query 
+ * OUTPUT: vettore di K-Nearest Neighbour float
+ */
+float* executeQuery(float q) {
+  return NULL;
+} 
+/**
+ * Calcola la distanza euclidea tra un punto referenziato da id e una distanza d
+ * INPUT: int id, id dell'elemento generico a cui è necessario accedere con v[i], float d, distanza 
+ * OUTPUT: float out, distanza calcolata
+ */
+float dEuclidea(int id, float d) {
+  return 0.0f;
+}
 int main() 
 {
 	return 0;
