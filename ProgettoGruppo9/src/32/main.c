168198e59a3ea97be58926b1158afe410e2b96cb
Kiriaevi <fra977502@gmail.com>
2025-11-18 21:09:05 +0100

    + aggiunta idea di implementazione per quantizing discussa in aula

--- ProgettoGruppo9/src/32/main.c
+++ ProgettoGruppo9/src/32/main.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
+static const int *arr_ctx;
 /**
  * Legge da un file e restituisce un array di float 
  * INPUT: string s, contenente il path da leggere 
@@ -9,13 +11,61 @@
 float* readFile(char *f) {
   return NULL;
 }
+int comparatore(const void *a, const void *b) {
+  int val1 = *(int*)a;
+  int val2 = *(int*)b;
+  return (abs(arr_ctx[val2]) - abs(arr_ctx[val1]));
+}
 /** 
  * INPUT: array v di dimensione D, parametro x che indica quanti massimi in v.assoluto cercare
  * OUTPUT: restituisce due array di interi v+ e v-, inizializzata con 0 e 1 nella posizione dei massimi trovati
  */
 void quantizing() {
+    // possibile FIXME: l'array potrebbe essere MOLTO grande, forse è meglio allocare
+    // nell'heap (malloc) e non nello stack
+    int arr[] = { 2 ,6, 1, -5, 3, 4 };
+    int n = sizeof(arr) / sizeof(arr[0]);
 
+    int array_indici[n];
+    // 0, 1, 2, 3, 4, 5
+    for(int i = 0; i < n; i++) 
+      array_indici[i] = i;
+
+    // mi aspetto di trovare 1, -3, 5, 4, 0, 2
+    arr_ctx = arr;
+    qsort(array_indici, n, sizeof(int), comparatore);
+    for (int i = 0; i < n; i++) {
+      if (arr[array_indici[i]] < 0)
+        array_indici[i] = -array_indici[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+      printf("%d\t", array_indici[i]);
+    }
+    // ora prendo i primi X 
+    // TODO: sostituire i numeri fittizzi con i parametri del metodo
+    int D = 5; //numero fittizio per ora
+    int x = 2; //numero fittizio per ora
+    if(x > D) {
+      fprintf(stderr, "ERRORE: hai richiesto %d massimi ma il dataset è grande al massimo %d", x, D);
+    }
+
+    for(int i = 0; i < x; i++) {
+      // se negativo allora scrivo su v-
+      int idx = array_indici[i];
+      //TODO: sostituire il commento in cui vengono assegnati i valori con vettori esistenti in output
+      if(idx < 0) {
+        //v_minus[-idx] = 1
+      } else {
+        //v_plus[idx] = 1
+      }
+      // nel nostro esempio specifico mi aspetto di trovare la terza posizione 
+      // di v_minus pari ad 1 e la prima e quinta posizione di v_plus pari ad 1
+    }
+
+    arr_ctx = NULL;
 }
+
 /**
  * ordina un array in valore assoluto
  * INPUT: arr, array di float da ordinare
@@ -60,5 +110,7 @@ float dEuclidea(int id, float d) {
 }
 int main() 
 {
-	return 0;
+
+  quantizing();
+  return 0;
 }
