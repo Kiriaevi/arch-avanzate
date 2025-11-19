741f937bd4762d2170b2c8d26821f46427f61f65
Christian <christianbruni131@gmail.com>
2025-11-19 17:14:24 +0100

    + correzione header

--- ProgettoGruppo9/src/32/include/operazioniVettoriali.h
+++ ProgettoGruppo9/src/32/include/operazioniVettoriali.h
@@ -1,5 +1,4 @@
 #pragma once
 
 float dEuclidea(float *v, float *w, const unsigned int D);
-void ordinamento();
-float prodScalare(float *v, float *w);
+float prodScalare(float *v, float *w, const unsigned int D);
