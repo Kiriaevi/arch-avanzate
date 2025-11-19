42fcab66198924b9377537705438c34402b1e7f0
Helgrind18 <fiorepasquale2003@gmail.com>
2025-11-19 15:12:32 +0100

    ~ spostamento metodi in altri file per più ordine

---
+++ ProgettoGruppo9/src/32/operazioniVettoriali.c
@@ -0,0 +1,24 @@
+/**
+ * Calcola la distanza euclidea tra un punto referenziato da id e una distanza d
+ * INPUT: int id, id dell'elemento generico a cui è necessario accedere con v[i], float d, distanza 
+ * OUTPUT: float out, distanza calcolata
+ */
+float dEuclidea(int id, float d) {
+  return 0.0f;
+}
+/**
+ * ordina un array in valore assoluto
+ * INPUT: arr, array di float da ordinare
+ * OUTPUT: outArr, array ordinato
+ */
+void ordinamento() {
+}
+
+/**
+ * Dati due vettori v e w ne effettua il prodotto scalare
+ * INPUT: array di float v, array di float w 
+ * OUTPUT: un float che restituisce la distanza
+ */
+float prodScalare(float *v, float *w) {
+  return 0.0f;
+}
