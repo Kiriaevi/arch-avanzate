a4bbaf2c2450d5293e08dd782df8610d45f78da2
Helgrind18 <fiorepasquale2003@gmail.com>
2025-11-22 15:43:31 +0100

    + branch testQuerying

--- ProgettoGruppo9/src/32/main.c
+++ ProgettoGruppo9/src/32/main.c
@@ -10,6 +10,7 @@ static int h = 1; // numero di pivot
 static int x = 3; // parametro di quantizzazione
 static int k = 3; // numero di vicini
 
+#define FLT_MAX 3.402823466e+38F
 
 //
 // ---------------------------------------------------------------
@@ -28,6 +29,75 @@ void quantizing(float *v, float *vMinus, float *vPlus);
 void testQuantizing(float *v, const char *name);
 int nomeFittizio();
 
+float* querying(float *query, float *pivot, float *dataSet);
+
+//
+// ---------------------------------------------------------------
+//  QUERYING
+// ---------------------------------------------------------------
+float* querying(float *query, float *pivot, float *dataSet){
+    // Il vettore KNN è lungo K elementi, ognuno a D dimensioni.
+    int dimensioneKnn = 2*k;
+    float* KNN = malloc(sizeof(float) * dimensioneKnn);
+    int j = 0;
+    // Inizializza i KNN di q a distanza +inf
+    for (int i = 0; i < dimensioneKnn; i++)
+    {
+        if (j % 2 == 0)
+        {
+            KNN[i] = -1;
+        }
+        else{
+            KNN[i] = FLT_MAX;
+        }
+        j = (j+1)%2;
+        
+    }
+    float* distanzaApprossimataQP = malloc(sizeof(float) * h); // Questo è lungo tanto quanto una riga del dataset
+    //Itero per ricostruire i pivot
+    int index = 0;
+    float* pivotCuscinetto = calloc(D, sizeof(float)); // alloco un vettore temporaneo tutto nullo, lo aggiorno ad ogni iterazione dei pivot
+    for (int i = 0; i < D * h; i+= D)
+    {
+        //Il ciclo esterno seleziona il pivot i esimo
+        for (int j = 0; j < D; j++)
+        {
+            // il ciclo interno, invece, seleziona elemento per elemento del pivot i esimo
+            pivotCuscinetto[j] = pivot[i + j];
+        }
+        distanzaApprossimataQP[index] = distanzaApprossimata(query,pivotCuscinetto);
+        index++;
+    }
+    // Precalcoliamo la distanza approssimata (vettoreDataset - pivot). È grande N elementi, uno per riga
+    float* distanzaApprossimataVP = malloc(sizeof(float) * N); 
+    // Creo un vettore cuscinetto per estrarre la riga dal dataset
+    float* vettoreCuscinetto = malloc(sizeof(float) * D);
+    for (int i = 0; i < D * N; i+=D)
+    {
+        for (int j = 0; j < D; j++)
+        {
+            vettoreCuscinetto[j] = dataSet[i+j];
+        }
+        // Ho estratto il vettore, ora calcolo la distanza approssimata da ogni pivot
+        // 
+        for (int indexPivot = 0; indexPivot < D * h; indexPivot+=D)
+        {
+            for (int elementoPivot = 0; elementoPivot < D; elementoPivot++)
+            {
+                
+            }
+            
+        }
+        
+        
+    }
+    free(vettoreCuscinetto);
+    free(distanzaApprossimataVP);
+    free(pivotCuscinetto);
+    free(distanzaApprossimataQP);
+    free(KNN);
+}
+
 
 //
 // ---------------------------------------------------------------
@@ -43,8 +113,8 @@ float* readFile(char *f) {
 //  CALCOLO PIVOT
 // ---------------------------------------------------------------
 float* calcoloPivot(float *dataSet, int h, int N, int D) {
-  float* pivot = malloc(sizeof(float) * D * h);
-    int offset = (int) floorf((float)N / h);
+  float* pivot = (float*)malloc(sizeof(float) * D * h);
+  int offset = (int) floorf((float)N / h);
   int k = 0;
 
     for (int i = 0; i < N && k < h; i += offset) {
@@ -217,19 +287,13 @@ float* creaVettore(float *v, int idx){
 }
 float* indexing(float *p, float *v) {
     float *output = malloc(N * h * sizeof(float));
-    
     for (int r = 0; r < N; r++) {
-
         float *vettore = &v[r * D];   // CAMBIA QUI
-
         for (int c = 0; c < h; c++) {
-
             float *vPivot = &p[c * D]; // E QUI
-
             output[r * h + c] = distanzaApprossimata(vettore, vPivot);
         }
     }
-
     return output;
 }
 //
@@ -359,8 +423,6 @@ void testIndexing() {
     free(IDX_app);
     free(IDX_real);
 }
-
-
 
 
 //
