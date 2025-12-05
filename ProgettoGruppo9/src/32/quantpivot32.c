0661d6f0d8d37db612996b6fcec3ab97f3244587
Helgrind18 <fiorepasquale2003@gmail.com>
2025-12-05 18:40:32 +0100

    tentativo di ottimizzazione: bitPacking

--- ProgettoGruppo9/src/32/quantpivot32.c
+++ ProgettoGruppo9/src/32/quantpivot32.c
@@ -8,11 +8,15 @@
 const int INDEXING_PROCEDURE_ERROR = -1;
 const int BLOCK_SIZE = 512;
 
+int numeroBlocchiBitPacking = 0;
+const int dimensioneSottovettoreBitPacking = 64;
+
 // Variabili globali per i dati quantizzati
 VECTOR vPlus_all = NULL;
 VECTOR vMinus_all = NULL;
 VECTOR pPlus = NULL;
 VECTOR pMinus = NULL;
+
 
 /* Ma che vuol dire che in C non c'è l'overloading della funzioni... -> https://en.cppreference.com/w/c/language/generic.html*/
 extern float prodScalaref(float *v, float *w, int D);
@@ -77,12 +81,13 @@ type get_d_k_max(VECTOR KNN, int k)
 }
 
 // Calcolo distanza approssimata (Eq. 2 del documento)
-type distanzaApprossimataPreQ(VECTOR vPlus, VECTOR vMinus, VECTOR wPlus, VECTOR wMinus, int D)
+type distanzaApprossimataPreQ(__uint64_t* vPlus, __uint64_t* vMinus, __uint64_t* wPlus, __uint64_t* wMinus)
 {
-  type posPos = prodScalare(vPlus, wPlus, D);
-  type negNeg = prodScalare(vMinus, wMinus, D);
-  type posNeg = prodScalare(vPlus, wMinus, D);
-  type negPos = prodScalare(vMinus, wPlus, D);
+  type posPos = andBitABit(vPlus, wPlus, numeroBlocchiBitPacking);
+  type negNeg = andBitABit(vMinus, wMinus, numeroBlocchiBitPacking);
+  type posNeg = andBitABit(vPlus, wMinus, numeroBlocchiBitPacking);
+  type negPos = andBitABit(vMinus, wPlus, numeroBlocchiBitPacking);
+  
   return posPos + negNeg - posNeg - negPos;
 }
 
@@ -98,20 +103,20 @@ VECTOR indexing(params* input)
 
   for (int r = 0; r < N; r++)
   {
-    VECTOR vPlus = &vPlus_all[r * input->D]; 
-    VECTOR vMinus = &vMinus_all[r * input->D];
+    __uint64_t* vPlus = &vPlus_all[r * numeroBlocchiBitPacking]; 
+    __uint64_t* vMinus = &vMinus_all[r * numeroBlocchiBitPacking];
     for (int c = 0; c < h; c++)
     {
-      VECTOR pPlusC = &pPlus[c * input->D];
-      VECTOR pMinusC = &pMinus[c * input->D];
-      output[r * h + c] = distanzaApprossimataPreQ(vPlus, vMinus, pPlusC, pMinusC, input->D);
+      __uint64_t* pPlusC = &pPlus[c * numeroBlocchiBitPacking];
+      __uint64_t* pMinusC = &pMinus[c * numeroBlocchiBitPacking];
+      output[r * h + c] = distanzaApprossimataPreQ(vPlus, vMinus, pPlusC, pMinusC);
     }
   }
   return output;
 }
 
 // Funzione di quantizzazione (versione HEAD)
-void quantizing(VECTOR v, VECTOR vMinus, VECTOR vPlus, params* input, int *array_indici)
+void quantizing(VECTOR v, __uint64_t* vMinus, __uint64_t* vPlus, params* input, int *array_indici, VECTOR vMinus_Cuscinetto, VECTOR vPlus_Cuscinetto)
 {
   int D = input->D;
   int x = input->x;
@@ -122,8 +127,8 @@ void quantizing(VECTOR v, VECTOR vMinus, VECTOR vPlus, params* input, int *array
   for (int k = 0; k < D; k++)
   {
     array_indici[k] = k;
-    vPlus[k] = 0;
-    vMinus[k] = 0;
+    vPlus_Cuscinetto[k] = 0.0;
+    vMinus_Cuscinetto[k] = 0.0;
   }
 
   // 2. Cerco gli X elementi con valore assoluto massimo (Partial Selection Sort)
@@ -157,13 +162,30 @@ void quantizing(VECTOR v, VECTOR vMinus, VECTOR vPlus, params* input, int *array
     // Se v >= 0 -> vPlus=1, altrimenti vMinus=1
     if (v[original_idx] >= 0) 
     {
-      vPlus[original_idx] = 1.0;
+      vPlus_Cuscinetto[original_idx] = 1.0;
     }
     else 
     {
-      vMinus[original_idx] = 1.0;
+      vMinus_Cuscinetto[original_idx] = 1.0;
     }
   }
+
+for (int i = 0; i < numeroBlocchiBitPacking; i++)
+{
+    for (int j = 0; j < dimensioneSottovettoreBitPacking; j++)  // ogni blocco ha max 64 bit
+    {
+        int idx = i * dimensioneSottovettoreBitPacking + j;
+        if (idx >= D) break;  // evita di andare oltre D
+
+        if (vPlus_Cuscinetto[idx] == 1.0)
+            vPlus[i] |= 1ULL << j;   // imposta il bit j del blocco i
+
+        if (vMinus_Cuscinetto[idx] == 1.0)
+            vMinus[i] |= 1ULL << j;  // imposta il bit j del blocco i
+    }
+}
+
+  
 }
 
 // Selezione Pivot
@@ -195,8 +217,8 @@ void preQuantizeDataset(params *input)
 {
   int N = input->N;
   int D = input->D;
-  vPlus_all = malloc((size_t)N * D * sizeof(type));
-  vMinus_all = malloc((size_t)N * D * sizeof(type));
+  vPlus_all = malloc((size_t)N * numeroBlocchiBitPacking * sizeof(__uint64_t));
+  vMinus_all = malloc((size_t)N * numeroBlocchiBitPacking * sizeof(__uint64_t));
 
   if (!vPlus_all || !vMinus_all) {
     fprintf(stderr, "Errore allocazione vPlus_all/vMinus_all\n");
@@ -207,16 +229,20 @@ void preQuantizeDataset(params *input)
   }
 
   int *idx_buff = malloc(D * sizeof(int));
+  VECTOR vPlus_Cuscinetto = calloc(D, sizeof(type));
+  VECTOR vMinus_Cuscinetto = calloc(D, sizeof(type));
 
   for (int i = 0; i < N; i++)
   {
-    VECTOR v = &input->DS[i * D];
-    VECTOR vp = &vPlus_all[i * D];
-    VECTOR vm = &vMinus_all[i * D];
-    quantizing(v, vm, vp, input, idx_buff);
+    VECTOR v = &input->DS[i * D]; // Vettore corrente del dataset
+    __uint64_t* vp = &vPlus_all[i * numeroBlocchiBitPacking]; // Vettore vPlus da quantizzare, preso dalla matrice globale
+    __uint64_t* vm = &vMinus_all[i * numeroBlocchiBitPacking]; // Vettore vMinus da quantizzare, preso dalla matrice globale
+    quantizing(v, vm, vp, input, idx_buff, vMinus_Cuscinetto, vPlus_Cuscinetto);
   }
 
   free(idx_buff);
+  free(vPlus_Cuscinetto);
+  free(vMinus_Cuscinetto);
 }
 
 // Pre-quantizzazione dei Pivot
@@ -237,97 +263,28 @@ void preQuantizePivots(params *input)
   }
 
   int *idx_buff = malloc(D * sizeof(int));
+  VECTOR vPlus_Cuscinetto = calloc(D, sizeof(type));
+  VECTOR vMinus_Cuscinetto = calloc(D, sizeof(type));
 
   for (int i = 0; i < h; i++)
   {
     int pivot_idx = input->P[i];
     if (pivot_idx < 0 || pivot_idx >= input->N) {
       // Gestione errore pivot fuori range
-      VECTOR pp = &pPlus[i * D];
-      VECTOR pm = &pMinus[i * D];
+      VECTOR pp = &pPlus[i * numeroBlocchiBitPacking];
+      VECTOR pm = &pMinus[i * numeroBlocchiBitPacking];
       for (int t = 0; t < D; t++) { pp[t]=0.0; pm[t]=0.0; }
       continue;
     }
     VECTOR p = &input->DS[pivot_idx * D];
-    VECTOR pp = &pPlus[i * D];
-    VECTOR pm = &pMinus[i * D];
-    quantizing(p, pm, pp, input, idx_buff);
+    __uint64_t* pp = &pPlus[i * numeroBlocchiBitPacking];
+    __uint64_t* pm = &pMinus[i * numeroBlocchiBitPacking];
+    quantizing(p, pm, pp, input, idx_buff, vMinus_Cuscinetto, vPlus_Cuscinetto);
   }
 
   free(idx_buff);
-}
-
-// Funzione di ricerca per singola query
-// NOTA: Firma aggiornata per accettare buffer esterni (qPlus, qMinus, etc.)
-void querying2(VECTOR query, params *input, VECTOR qPlus, VECTOR qMinus, VECTOR dQP, int *array_indici, VECTOR KNN)
-{
-  int D = input->D;
-  int h = input->h;
-  int k = input->k;
-  int N = input->N;
-
-  // 1. Quantizza la query
-  quantizing(query, qMinus, qPlus, input, array_indici);
-
-  // 2. Calcola d(q,p) per ogni pivot 
-  for (int j = 0; j < h; j++)
-  {
-    VECTOR pPlusC = &pPlus[j * D]; 
-    VECTOR pMinusC = &pMinus[j * D];
-    dQP[j] = distanzaApprossimataPreQ(qPlus, qMinus, pPlusC, pMinusC, D);
-  }
-
-  // 3. Inizializza K-NN 
-  for (int i = 0; i < k; i++)
-  {
-    KNN[2 * i] = -1.0f;     // ID
-    KNN[2 * i + 1] = INFINITY; // Distanza
-  }
-
-  // 4. Itera sul dataset 
-  for (int i = 0; i < N; i++)
-  {
-    // A. Calcolo Lower Bound coi Pivot 
-    type best_lb = 0.0;
-    for (int j = 0; j < h; j++)
-    {
-      type d_vi_pj = input->index[i * h + j]; // Accesso all'indice precalcolato
-      type lb = ABS(d_vi_pj - dQP[j]);
-      if (lb > best_lb)
-        best_lb = lb;
-    }
-
-    // Recupera la distanza massima attuale nel K-NN
-    type d_k_max = get_d_k_max(KNN, k);
-
-    // B. Filtro Pivot 
-    if (best_lb >= d_k_max)
-      continue;
-
-    // C. Calcolo Distanza Approssimata 
-    VECTOR vPlus = &vPlus_all[i * D];
-    VECTOR vMinus = &vMinus_all[i * D];
-    type d_q_v_approx = distanzaApprossimataPreQ(qPlus, qMinus, vPlus, vMinus, D);
-
-    // D. Inserimento basato su Distanza Approssimata 
-    if (d_q_v_approx < d_k_max)
-    {
-      insert_into_knn(KNN, k, i, d_q_v_approx);
-    }
-  }
-
-  // 5. Raffinamento Finale (Ricalcolo Euclideo - LOGICA HEAD)
-  // Solo ora calcoliamo la distanza reale per i vincitori
-  for (int i = 0; i < k; i++)
-  {
-    int id_vicino = (int)KNN[2 * i];
-
-    if (id_vicino >= 0) {
-      VECTOR v = &input->DS[id_vicino * D];
-      // Sostituiamo la distanza approssimata con quella reale
-      KNN[2 * i + 1] = dEuclidea(query, v, D); 
-    }
-  }
+  free(vPlus_Cuscinetto);
+  free(vMinus_Cuscinetto);
 }
 
 // Processa un blocco di dataset [start_N, end_N) per una specifica query
@@ -359,9 +316,9 @@ void process_block_for_query(int start_N, int end_N, VECTOR query, params *input
     if (best_lb >= d_k_max)
       continue;
 
-    VECTOR vPlus = &vPlus_all[i * D];
-    VECTOR vMinus = &vMinus_all[i * D];
-    type d_q_v_approx = distanzaApprossimataPreQ(qPlus, qMinus, vPlus, vMinus, D);
+    __uint64_t* vPlus = &vPlus_all[i * numeroBlocchiBitPacking];
+    __uint64_t* vMinus = &vMinus_all[i * numeroBlocchiBitPacking];
+    type d_q_v_approx = distanzaApprossimataPreQ(qPlus, qMinus, vPlus, vMinus);
 
     // D. Inserimento
     if (d_q_v_approx < d_k_max)
@@ -371,6 +328,9 @@ void process_block_for_query(int start_N, int end_N, VECTOR query, params *input
   }
 }
 void fit(params* input){
+  
+  numeroBlocchiBitPacking = ((input->D + 63) / 64); // arrotondamento a multiplo di 64
+
   // Selezione dei pivot
   printf("INIZIO SELEZIONE PIVOT\n");
   input->P = calcoloPivot(input->DS, input->h, input->N, input->D);
@@ -427,17 +387,19 @@ void predict(params* input){
 
   // Allocazione buffer temporanei FUORI dal ciclo per performance
   // Questo abilita la logica usata in querying2
-  VECTOR qPlusAll = malloc(D * nq * sizeof(type));
-  VECTOR qMinusAll = malloc(D * nq * sizeof(type));
+  __uint64_t* qPlusAll = malloc(numeroBlocchiBitPacking * nq * sizeof(__uint64_t));
+  __uint64_t* qMinusAll = malloc(numeroBlocchiBitPacking * nq * sizeof(__uint64_t));
   VECTOR dQPAll = malloc(h * nq * sizeof(type));
 
   VECTOR KNNAll = malloc(2*k * nq *  sizeof(type));
 
   int *idx_buff = malloc(D * sizeof(int)); 
+  VECTOR qPlus_Cuscinetto = calloc(D, sizeof(type));
+  VECTOR qMinus_Cuscinetto = calloc(D, sizeof(type));
   for(int q = 0; q < nq; q++) {
     VECTOR query = &input->Q[q * D];
-    VECTOR qPlus = &qPlusAll[q * D];
-    VECTOR qMinus = &qMinusAll[q * D];
+    __uint64_t* qPlus = &qPlusAll[q * numeroBlocchiBitPacking];
+    __uint64_t* qMinus = &qMinusAll[q * numeroBlocchiBitPacking];
     VECTOR dQP = &dQPAll[q * h];
     VECTOR currentKNN = &KNNAll[q * 2 * k];
 
@@ -449,16 +411,18 @@ void predict(params* input){
     }
 
     // Quantizza query
-    quantizing(query, qMinus, qPlus, input, idx_buff);
+    quantizing(query, qMinus, qPlus, input, idx_buff, qMinus_Cuscinetto, qPlus_Cuscinetto);
 
     // Precalcola distanze query-pivot
     for (int j = 0; j < h; j++) {
       VECTOR pPlusC = &pPlus[j * D]; 
       VECTOR pMinusC = &pMinus[j * D];
-      dQP[j] = distanzaApprossimataPreQ(qPlus, qMinus, pPlusC, pMinusC, D);
+      dQP[j] = distanzaApprossimataPreQ(qPlus, qMinus, pPlusC, pMinusC);
     }
   } 
   free(idx_buff);
+  free(qPlus_Cuscinetto);
+  free(qMinus_Cuscinetto);
 
   // iteriamo i blocchi di dataset in ram e calcoliamo tutte le query
 #pragma omp parallel for
